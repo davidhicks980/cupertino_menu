2fdf66a95ea26d86a9d0b5a95454ed7f6728631e
David Hicks <davidhicks980@gmail.com>
2024-02-18 20:50:17 -0500

    Examples finished

--- lib/cupertino_menu_anchor.2.dart
+++ lib/cupertino_menu_anchor.2.dart
@@ -1,82 +1,143 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-/// Flutter code sample for [MenuAnchor].
+/// A demonstration showing how to use [CupertinoMenuAnchor] to create a
+/// navigation history menu similar to the navigation history stack on iOS.
 
+/// Flutter code sample for [CupertinoMenuAnchor].
 void main() => runApp(const CupertinoNestedApp());
 
+/// A wrapper that shows a navigation history menu when the back button is
+/// pressed down.
 class MenuBackButtonWrapper extends StatelessWidget {
-  const MenuBackButtonWrapper({super.key, required this.child});
+  const MenuBackButtonWrapper({
+    super.key,
+    required this.viewCount,
+    required this.child,
+  });
+
+  final int viewCount;
   final Widget child;
 
-  void _popToRoute(BuildContext context, String routeName) {
+  void _popToRoute(BuildContext context, int viewIndex) {
     Navigator.popUntil(
       context,
       (Route<dynamic> route) {
-        return route.settings.name == routeName;
+        return route.settings.name == 'View $viewIndex' ||
+            route.settings.name == '/';
       },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoMenuAnchor(
-      menuChildren: <Widget>[
-        CupertinoMenuItem(
-          child: const Text('Home'),
-          onPressed: () {
-            _popToRoute(context, HomePage.route);
-          },
-        ),
-        if (ModalRoute.of(context)?.settings.name == DetailPage.route)
-          CupertinoMenuItem(
-            child: const Text('Detail'),
-            onPressed: () {
-              _popToRoute(context, SummaryPage.route);
-            },
-          ),
-      ],
-      builder: (
-        BuildContext context,
-        CupertinoMenuController controller,
-        Widget? child,
-      ) {
-        return GestureDetector(
-          onLongPressStart: (_) {
-            if (controller.menuStatus
-                case MenuStatus.opening || MenuStatus.opened) {
-              controller.close();
-            } else {
-              controller.open();
-            }
-          },
-          child: child,
-        );
+    final CupertinoMenuController controller = CupertinoMenuController();
+
+    // Close the menu if the route is popped.
+    return PopScope(
+      onPopInvoked: (bool popped) {
+        if (popped) {
+          controller.close();
+        }
       },
-      child: child,
+      child: CupertinoMenuAnchor(
+        controller: controller,
+        menuChildren: <Widget>[
+          for (int i = 1; i < viewCount; i++)
+            CupertinoMenuItem(
+              child: Text('View $i'),
+              onPressed: () {
+                _popToRoute(context, i);
+              },
+            ),
+        ],
+        builder: (
+          BuildContext context,
+          CupertinoMenuController controller,
+          Widget? child,
+        ) {
+          return GestureDetector(
+            // Long press can't be used here because it would cancel the pan
+            // gesture.
+            onTapDown: (TapDownDetails details) {
+              if (controller.menuStatus
+                  case MenuStatus.opening || MenuStatus.opened) {
+                controller.close();
+              } else {
+                controller.open();
+              }
+            },
+            child: child,
+          );
+        },
+        child: child,
+      ),
     );
   }
 }
 
-class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
-  static String route = '/nested';
+/// A view that pushes itself onto the navigation stack.
+class RecursiveView extends StatelessWidget {
+  const RecursiveView({super.key, required this.depth});
+  final int depth;
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        leading: MenuBackButtonWrapper(
-            child: CupertinoNavigationBarBackButton(
-          previousPageTitle: 'Home',
+    Widget? leading;
+    if (depth != 0) {
+      // Wrap the back button with a menu that shows the navigation history.
+      leading = MenuBackButtonWrapper(
+        viewCount: depth,
+        child: CupertinoNavigationBarBackButton(
+          previousPageTitle: 'View ${depth - 1}',
           onPressed: () {
             Navigator.maybePop(context);
           },
-        )),
-        middle: const Text('Nested Route'),
+        ),
+      );
+    }
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        leading: leading,
+        middle: Text('View $depth'),
       ),
-      child: const Placeholder(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const Text(
+            '''Push some views and long press the '''
+            '''\nback button to show a navigation history menu.''',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: CupertinoColors.systemGreen,
+            ),
+          ),
+          const SizedBox(height: 32),
+          Center(
+            child: CupertinoButton.filled(
+              child: Text('Push View ${depth + 1}'),
+              onPressed: () {
+                _pushNextPage(context);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _pushNextPage(BuildContext context) {
+    final int nextDepth = depth + 1;
+    Navigator.of(context).push(
+      CupertinoPageRoute<void>(
+        settings: RouteSettings(name: 'View $nextDepth'),
+        builder: (BuildContext context) {
+          return RecursiveView(depth: nextDepth);
+        },
+      ),
     );
   }
 }
@@ -85,64 +146,10 @@ class CupertinoNestedApp extends StatelessWidget {
   const CupertinoNestedApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-        localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+    return const CupertinoApp(
+        localizationsDelegates: <LocalizationsDelegate<dynamic>>[
           DefaultMaterialLocalizations.delegate,
         ],
-        routes: <String, WidgetBuilder>{
-          HomePage.route: (BuildContext context) => const HomePage(), // "/"
-          SummaryPage.route: (BuildContext context) => const SummaryPage(), // "/detail"
-          DetailPage.route: (BuildContext context) => const DetailPage(), // "/nested"
-        });
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  static String route = '/';
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Center(
-        child: CupertinoButton(
-          child: Text('Go to $SummaryPage'),
-          onPressed: () {
-            Navigator.of(context).pushNamed(SummaryPage.route);
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class SummaryPage extends StatelessWidget {
-  const SummaryPage({super.key});
-
-  static String route = '/summary';
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        leading: MenuBackButtonWrapper(
-            child: CupertinoNavigationBarBackButton(
-          previousPageTitle: 'Home',
-          onPressed: () {
-            Navigator.maybePop(context);
-          },
-        )),
-        middle: Text('$SummaryPage'),
-      ),
-      child: Center(
-        child: CupertinoButton(
-          child: Text('Go to $DetailPage'),
-          onPressed: () {
-            Navigator.of(context).pushNamed(DetailPage.route);
-          },
-        ),
-      ),
-    );
+        home: RecursiveView(depth: 0));
   }
 }
